248c541d37da0d177acb3d318eb17217b733df64
neverest-westmont <neverest@westmont.edu>
2022-11-11 20:19:23 -0800

    add Item table

--- potluck/mainapp/admin.py
+++ potluck/mainapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Potluck
+from .models import Potluck, Item
 
 admin.site.register(Potluck)
+admin.site.register(Item)
--- potluck/mainapp/models.py
+++ potluck/mainapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 class Potluck(models.Model):
-    name = models.CharField(max_length=200)
-    date = models.DateTimeField('potluck date')
+    name = models.CharField(max_length=64)
+    date = models.DateTimeField()
+    host = models.CharField(max_length=64, blank=True, null=True)
     def __str__(self):
         return self.name
+    
+class Item(models.Model):
+    potluck = models.ForeignKey(Potluck, on_delete=models.CASCADE)
+    item = models.CharField(max_length=64, blank=True, null=True)
+    def __str__(self):
+        return self.item
