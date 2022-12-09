130444af0d63b8d631e45658e36dc1377407d427
Todd Knight <91100707+tknight20@users.noreply.github.com>
2022-12-08 17:05:59 -0800

    More lightsail changes

--- potluck/potluck/settings.py
+++ potluck/potluck/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'mysql',
         'USER': 'root',
         'PASSWORD': 'cookie',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 3306,
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
