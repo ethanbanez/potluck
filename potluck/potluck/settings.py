086a14103d3078943edf68cbd56b0c58b5208580
Todd Knight <91100707+tknight20@users.noreply.github.com>
2022-12-08 17:15:43 -0800

    Update settings.py

--- potluck/potluck/settings.py
+++ potluck/potluck/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'mysql',
         'USER': 'root',
         'PASSWORD': 'cookie',
-        'HOST': 'localhost',
+        'HOST': '0.0.0.0',
         'PORT': 3306,
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
