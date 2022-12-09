040476c3c7ec410b996a565ab9f1ea782b279d46
tknight20 <tknight@westmont.edu>
2022-12-08 17:50:53 -0800

    environment variable shenanigans

--- potluck/potluck/settings.py
+++ potluck/potluck/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,7 +83,7 @@ DATABASES = {
         'NAME': 'mysql',
         'USER': 'root',
         'PASSWORD': 'cookie',
-        'HOST': '0.0.0.0',
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': 3306,
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
